627db0b89fa02714bf253673082a981f674b27d3
duyphuong0103 <nguyenthiduyphuong0103@gmail.com>
2025-10-15 08:41:21 +0700

    push code

--- app/src/main/java/com/example/project_gk_ntdp/screen_2.java
+++ app/src/main/java/com/example/project_gk_ntdp/screen_2.java
@@ -1,4 +1,14 @@
 package com.example.project_gk_ntdp;
 
-public class screen_2 {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class screen_2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.screen_2);}
 }
